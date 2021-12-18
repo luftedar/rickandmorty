08f166ef2c2d969e4bd77c86005d696f6fe69fe6
luftedar <ugurorcun91@gmail.com>
2021-12-18 14:50:28 +0300

    Add Initial Character State

--- src/Redux/characters/characters.js
+++ src/Redux/characters/characters.js
@@ -1,1 +1,12 @@
 import * as apiCalls from '../../Api/api';
+
+const FETCH_CHARACTERS = 'charactersStore/characters/FETCH_CHARACTERS';
+const initialState = [];
+
+export const fetchAllCharacters = (payload) => async (dispatch) => {
+  await apiCalls.getAllCharacters(payload);
+  dispatch({
+    type: FETCH_CHARACTERS,
+    payload,
+  });
+};
