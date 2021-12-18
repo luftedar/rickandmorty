342ac71eaafc9f88555f851280963d44de1d3444
luftedar <ugurorcun91@gmail.com>
2021-12-18 14:57:12 +0300

    Imported Redux Store to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
+import store from './Redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
