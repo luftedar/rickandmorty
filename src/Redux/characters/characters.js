3f0c58ec3f4bd3ed3a37a6be8cf836c45bf95960
luftedar <ugurorcun91@gmail.com>
2021-12-18 14:52:25 +0300

    Add Characters Reducer

--- src/Redux/characters/characters.js
+++ src/Redux/characters/characters.js
@@ -10,3 +10,12 @@ export const fetchAllCharacters = (payload) => async (dispatch) => {
     payload,
   });
 };
+
+const charactersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_CHARACTERS:
+      return [...state, ...action.payload];
+    default:
+      return state;
+  }
+}
