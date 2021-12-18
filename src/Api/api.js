e4c217897914c3058c437da0116dae3bb9626580
luftedar <ugurorcun91@gmail.com>
2021-12-18 14:39:38 +0300

    Add getAllCharacters Fetch API

--- src/Api/api.js
+++ src/Api/api.js
@@ -2,3 +2,9 @@ const APIURL = 'https://rickandmortyapi.com/api';
 const CHARACTERS = 'character';
 const LOCATIONS = 'location';
 const EPISODES = 'episode';
+
+export const getAllCharacters = async () => {
+  const req = await fetch(`${APIURL}/${CHARACTERS}`)
+  const apiCharacters = await req.json();
+  return apiCharacters;
+}
