7e6ef321de777758e0b20570c6b92e66331702c4
luftedar <ugurorcun91@gmail.com>
2021-12-18 14:58:08 +0300

    Wrapped App Component with Provider with Redux Store

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import store from './Redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
