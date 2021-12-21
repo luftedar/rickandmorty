07c8ce1cfeb00b901c876a91140f3e57e1e79775
luftedar <ugurorcun91@gmail.com>
2021-12-21 17:48:41 +0300

    Updated Episodes Component

---
+++ src/Components/Episodes.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchAllEpisodes } from "../Redux/episodes/episodes";
+import Episode from "./Episode";
+
+function Episodes() {
+  const dispatch = useDispatch();
+  const episodes = useSelector((state) => state.episodesReducer);
+  useEffect(() => { dispatch(fetchAllEpisodes()); }, []);
+  return (
+    <div>
+      <ul>
+        {episodes.map((episode) => {
+          <Episode
+          key={episode.id}
+          episode={episode}
+          />
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Episodes
