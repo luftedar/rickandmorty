9a8dfbdc417b4c9cadb297aa91e46adc2d91c464
luftedar <ugurorcun91@gmail.com>
2021-12-21 17:15:54 +0300

    Update Location Action & Reducer

---
+++ src/Redux/locations/locations.js
@@ -0,0 +1,24 @@
+import * as apiCalls from '../../Api/api';
+
+const FETCH_LOCATIONS = 'charactersStore/characters/FETCH_LOCATIONS';
+const initialState = [];
+
+export const fetchAllLocations = () => async (dispatch) => {
+  const apiData = await apiCalls.getAllLocations();
+  const payload = apiData.results;
+  dispatch({
+    type: FETCH_CHARACTERS,
+    payload,
+  });
+};
+
+const locationsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_LOCATIONS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default locationsReducer;
