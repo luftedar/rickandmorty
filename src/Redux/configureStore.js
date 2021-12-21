542d01bd494bf9f2f56751976786ab376d32aca1
luftedar <ugurorcun91@gmail.com>
2021-12-21 17:18:32 +0300

    Created Episode Store

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import charactersReducer from './characters/characters';
-import locationsReducer from './locations/locations';
+import locationsReducer from './locations/locations';,
+import episodesReducer from './episodes/episodes';
 
 const reducer = combineReducers({
   charactersReducer,
   locationsReducer,
+  episodesReducer,
 });
 
 const store = createStore(
