f0c5b612b868bdd0b748229e3276e329112bbbe1
luftedar <ugurorcun91@gmail.com>
2021-12-22 00:49:33 +0300

    Deleted API call // Add Action Caller For Get Single Character

--- src/Redux/characters/characters.js
+++ src/Redux/characters/characters.js
@@ -1,6 +1,7 @@
 import * as apiCalls from '../../Api/api';
 
 const FETCH_CHARACTERS = 'charactersStore/characters/FETCH_CHARACTERS';
+const GET_SINGLE_CHARACTER = 'charactersStore/characters/GET_SINGLE_CHARACTER';
 const initialState = [];
 
 export const fetchAllCharacters = (page) => async (dispatch) => {
@@ -11,6 +12,11 @@ export const fetchAllCharacters = (page) => async (dispatch) => {
     payload,
   });
 };
+
+export const getSingleCharacter = (id) => ({
+  type: GET_SINGLE_CHARACTER,
+  id,
+});
 
 const charactersReducer = (state = initialState, action) => {
   switch (action.type) {
