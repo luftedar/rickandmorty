c4464b732d4f1ec767bd896b2e657ac87f01186b
luftedar <ugurorcun91@gmail.com>
2021-12-22 02:04:40 +0300

    Render Data in Character Details Component

--- src/Components/Characterdetails.js
+++ src/Components/Characterdetails.js
@@ -1,14 +1,27 @@
+/* eslint-disable */
 import { useSelector } from 'react-redux';
 
 function Characterdetails() {
   const pageURL = window.location.href;
   const pageID = parseInt(pageURL.split('/')[4], 10);
   const characters = useSelector((state) => (state.charactersReducer));
-  const trueCharacter = characters.filter((character) => character.id === pageID);
-  console.log(trueCharacter);
+  const trueCharacter = (characters.filter((character) => character.id === pageID))[0];
   return (
     <div>
-      Deneme
+      Character Name:
+      {trueCharacter.name}
+      Character Status:
+      {trueCharacter.status}
+      Character Species:
+      {trueCharacter.species}
+      Character Gender:
+      {trueCharacter.gender}
+      Character Origin:
+      {trueCharacter.origin.name}
+      Character Location:
+      {trueCharacter.location.name}
+      Character Episodes:
+      {trueCharacter.episode.length}  
     </div>
   );
 }
