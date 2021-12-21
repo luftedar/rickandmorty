291b5a4dd263edda03112fff004f9bf3e3a1ab72
luftedar <ugurorcun91@gmail.com>
2021-12-21 15:31:10 +0300

    Add Page Argument to fetchAllCharacters Action

--- src/Redux/characters/characters.js
+++ src/Redux/characters/characters.js
@@ -3,8 +3,8 @@ import * as apiCalls from '../../Api/api';
 const FETCH_CHARACTERS = 'charactersStore/characters/FETCH_CHARACTERS';
 const initialState = [];
 
-export const fetchAllCharacters = () => async (dispatch) => {
-  const apiData = await apiCalls.getAllCharacters();
+export const fetchAllCharacters = (page) => async (dispatch) => {
+  const apiData = await apiCalls.getAllCharacters(page);
   const payload = apiData.results;
   dispatch({
     type: FETCH_CHARACTERS,
