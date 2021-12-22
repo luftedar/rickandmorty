c30b18df15969103fccb845bab76449720b7de97
luftedar <ugurorcun91@gmail.com>
2021-12-22 12:34:19 +0300

    Completed Styling For Characters Page

--- src/Components/Character.js
+++ src/Components/Character.js
@@ -6,13 +6,13 @@ function Character({ character }) {
     <li className="character-item" key={character.id}>
       <img src={character.image} alt={character.name} className="character-image"></img>
       <div className="character-container">
-        <div className="card-description d-flex s-between a-center">
+        <div className="card-description d-flex-col">
           <div>
             Name: {character.name}
             <br />
             Status: {character.status}
           </div>
-          <div>
+          <div className="card-button">
             <Link to={`/character/${character.id}`}>More</Link>
           </div>
         </div>
