9629cc1f3d46ab19899c70a4762de45a62f0a5f0
luftedar <ugurorcun91@gmail.com>
2021-12-22 17:22:06 +0300

    Remove Eslint Disabler on Character Component // Fix Eslint Errors

--- src/Components/Character.js
+++ src/Components/Character.js
@@ -1,16 +1,20 @@
-/* eslint-disable */
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 function Character({ character }) {
   return (
     <li className="character-item" key={character.id}>
-      <img src={character.image} alt={character.name} className="character-image"></img>
+      <img src={character.image} alt={character.name} className="character-image" />
       <div className="character-container">
         <div className="card-description d-flex-col">
           <div>
-            Name: {character.name}
+            Name:
+            {' '}
+            {character.name}
             <br />
-            Status: {character.status}
+            Status:
+            {' '}
+            {character.status}
           </div>
           <div className="card-button">
             <Link to={`/character/${character.id}`}>More</Link>
@@ -20,5 +24,14 @@ function Character({ character }) {
     </li>
   );
 }
+
+Character.propTypes = {
+  character: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Character;
